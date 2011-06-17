d59a17aaeb664a6a9165467ee085a9bfb97909ab
sthysel <sthysel@gmail.com>
2011-06-17 19:54:11 +0200

    Introduced fairly arb distinction between store and factory... for s&g

--- src/main/java/thyscom/spaceinvaders/SpriteStore.java
+++ src/main/java/thyscom/spaceinvaders/SpriteStore.java
@@ -1,20 +1,6 @@
 package thyscom.spaceinvaders;
 
-import java.awt.Color;
-import java.awt.Graphics2D;
-import java.awt.GraphicsConfiguration;
-import java.awt.GraphicsEnvironment;
-import java.awt.HeadlessException;
-import java.awt.Image;
-import java.awt.RenderingHints;
-import java.awt.Transparency;
-import java.awt.geom.Ellipse2D;
-import java.awt.image.BufferedImage;
-import java.io.IOException;
-import java.net.URL;
 import java.util.HashMap;
-
-import javax.imageio.ImageIO;
 import org.apache.log4j.Logger;
 
 /**
@@ -35,10 +21,14 @@ public class SpriteStore {
     private static SpriteStore single = new SpriteStore();
     /** The cached sprite map, from reference to sprite instance */
     private HashMap<String, Sprite> sprites = new HashMap<String, Sprite>();
-
+    private SpriteFactory factory;
+    
+    private SpriteStore() {
+        factory = new SpriteFactory();
+    }
+    
     /**
      * Get the single instance of this class 
-     * 
      * @return The single instance of this class
      */
     public static SpriteStore getInstance() {
@@ -55,66 +45,9 @@ public class SpriteStore {
         // if we've already got the sprite in the cache
         // then just return the existing version
         if (!sprites.containsKey(ref)) {
-            addNewSprite(ref);
+            Sprite sprite = factory.getSprite(ref);
+            sprites.put(ref, sprite);
         }
         return sprites.get(ref);
-    }
-
-    /**
-     * Add a new sprite to the store.
-     * @param ref
-     * @throws HeadlessException 
-     */
-    private void addNewSprite(String ref) {
-        Image image = null;
-        try {
-            // The ClassLoader.getResource() ensures we get the sprite
-            // from the appropriate place, this helps with deploying the game
-            // with things like webstart. You could equally do a file look
-            // up here.
-            URL url = this.getClass().getClassLoader().getResource(ref);
-
-            // if the source file is available, use it. Else create a figting dot 
-            // on the spot.
-            if (url != null) {
-                image = loadImageFromDisk(url);
-            } else {
-                image = makeSubstituteImage();
-            }
-        } catch (IOException e) {
-            logger.error(e);
-            image = makeSubstituteImage();
-        }
-
-        // create a sprite, add it the cache then return it
-        Sprite sprite = new Sprite(image);
-        sprites.put(ref, sprite);
-    }
-
-    private Image loadImageFromDisk(URL url) throws IOException {
-        BufferedImage sourceImage = sourceImage = ImageIO.read(url);
-        // create an accelerated image of the right size to store our sprite in
-        Image image = getGraphicsConfiguration().createCompatibleImage(sourceImage.getWidth(), sourceImage.getHeight(), Transparency.BITMASK);
-        // draw our source image into the accelerated image
-        image.getGraphics().drawImage(sourceImage, 0, 0, null);
-
-        return image;
-    }
-
-    private Image makeSubstituteImage() {
-        int WIDTH = 20;
-        int HEIGHT = 20;
-        GraphicsConfiguration gc = getGraphicsConfiguration();
-        BufferedImage image = gc.createCompatibleImage(WIDTH, HEIGHT, Transparency.BITMASK);
-        Graphics2D g2d = image.createGraphics();
-        g2d.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
-        g2d.setColor(Color.GREEN);
-        g2d.fill(new Ellipse2D.Double(0, 0, WIDTH, WIDTH));
-        g2d.dispose();
-        return image;
-    }
-
-    private GraphicsConfiguration getGraphicsConfiguration() throws HeadlessException {
-        return GraphicsEnvironment.getLocalGraphicsEnvironment().getDefaultScreenDevice().getDefaultConfiguration();
     }
 }
